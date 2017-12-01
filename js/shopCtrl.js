be1e406b1a2d3c4682d9874299e650b12ca24c41
Christopher <thorstedc266935@gmail.com>
2017-12-01 15:35:19 -0700

    finished for real forgot to add JS folder to staging

---
+++ js/shopCtrl.js
@@ -0,0 +1,10 @@
+app.controller('shopCtrl', function ($scope, shopSrvc) {
+
+	shopSrvc.getProducts().then(function(results){
+        $scope.products = results;
+    })
+
+	$scope.clpr = (id) => {
+		$scope.prodinfo = shopSrvc.getProdDet(id);
+	}
+})
